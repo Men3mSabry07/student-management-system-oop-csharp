d0189cbf482c1585c63e52b26669d6f7c8dc95b5
Menem Sabry <men3m77sabry@gmail.com>
2026-02-07 02:00:18 +0200

    Add files via upload

---
+++ StudentManagementSystem/src/Instructor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementSystem
+{
+    class Instructor
+    {
+        public int InstructorId;
+        public string Name;
+        public string Specialization;
+
+        public Instructor(int instructorId, string name, string specialization)
+        {
+            this.InstructorId = instructorId;
+            this.Name = name;
+            this.Specialization = specialization;
+        }
+        // Methods
+        public string PrintDetails()
+        {
+            return $"ID: {InstructorId}, Name: {Name}, Specialization: {Specialization}";
+        }
+    }
+}
---
+++ StudentManagementSystem/src/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementSystem
+{
+    class Student
+    {
+        public int StudentId {get; set;}
+        public string Name {get; set;}
+        private int _age; 
+        public int Age
+        {
+            get {return _age;} 
+            set {_age = value;} 
+        }
+        public List<Course> Courses = new List<Course>();
+
+        public Student(int studentId, string name, int age)
+        {
+            this.StudentId = studentId;
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public bool Enroll(Course course)
+        {
+            if (Courses.Any(C => C.Title == course.Title)) return false;
+            else Courses.Add(course);
+
+            return true;
+        }
+        public string PrintDetails()
+        {
+            string CoursesList = string.Join(",",Courses.Select(C => C.Title).ToList());
+
+            return $"StudentId: {StudentId} , Name: {Name} , Age: {Age} , {CoursesList}";
+        }
+    }
+}
